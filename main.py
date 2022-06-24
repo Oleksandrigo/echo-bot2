c23f8df27c1a9e8f40efe6c2e0d60f63153a05cc
Oleksandrigo <oleksandrigo@gmail.com>
2022-06-24 10:08:39 +0300

    Initial commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = 8000
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
